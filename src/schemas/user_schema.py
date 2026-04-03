a17c7f7a3ed6bd2a7466f629185653c7c8f8c36f
Arvind-Vishwa <av0216380@gmail.com>
2026-04-04 02:34:37 +0530

    updated the user schema

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -1,12 +1,23 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 class UserSignup(BaseModel):
     email: EmailStr
     password: str
 
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        print("Password length:", len(value.encode("utf-8")))  # DEBUG
+
+        if len(value.encode("utf-8")) > 72:
+            raise ValueError("Password too long (max 72 bytes)")
+        return value
+
+
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
 
 class UserResponse(BaseModel):
     id: str
