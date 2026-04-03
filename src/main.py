758f9631fba6c1414e536a64fb6c6b9e980c0dba
Arvind-Vishwa <av0216380@gmail.com>
2026-04-04 00:02:33 +0530

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from src.api import auth_routes
+from src.core.dependencies import get_current_user
+from fastapi import Depends
 
 app = FastAPI()
 
+app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
+
 @app.get("/")
 def home():
-    return {"message": "E-commerce API is running"}
+    return {"message": "API running"}
+
+
+@app.get("/profile")
+def profile(user = Depends(get_current_user)):
+    return {
+        "message": "Protected route working",
+        "user": str(user["_id"])
+    }
