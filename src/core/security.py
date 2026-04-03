67f37112158528f7d6d3f575a0e368d868082ff6
Arvind-Vishwa <av0216380@gmail.com>
2026-04-04 00:01:43 +0530

    security updated

---
+++ src/core/security.py
@@ -0,0 +1,27 @@
+from passlib.context import CryptContext
+from jose import jwt
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Hash password
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# Verify password
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+# Create JWT token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(hours=24)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
