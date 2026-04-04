db63bb360dd07106ce9e324a36ff1fa2281fab4d
Arvind-Vishwa <av0216380@gmail.com>
2026-04-04 08:56:10 +0530

    updated main

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from src.api import auth_routes
 from src.core.dependencies import get_current_user
 from fastapi import Depends
 from src.api import product_routes
-
+from src.api import cart_routes
+from src.api import order_routes
 app = FastAPI()
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
@@ -23,3 +24,10 @@ def profile(user = Depends(get_current_user)):
 
 
 app.include_router(product_routes.router, prefix="/products", tags=["Products"])
+
+
+app.include_router(cart_routes.router)
+
+
+
+app.include_router(order_routes.router)
